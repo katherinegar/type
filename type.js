6451a37fc4b2cd8fddf571760766f31fc03a80cb
katherinegar <55204817+katherinegar@users.noreply.github.com>
2019-10-30 08:49:09 -0400

    Add files via upload

--- type.js
+++ type.js
@@ -1,6 +1,5 @@
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
   function playSound(e, clicked) {
     if (!e.keyCode) e.keyCode = clicked;
@@ -13,14 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => key.classList.remove("playing"), 150); 
   }
 
-  function handleClick(e) {
-    const btn = e.target.tagName === "DIV" ? e.target : e.target.parentElement;
-    playSound(e, btn.getAttribute("data-key"));
-  }
-
   window.addEventListener("keydown", playSound);
   const btns = document.getElementsByClassName("key");
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", handleClick);
-  }
+  for (let i = 0; i < btns.length; i++);
 });
+
+// credit taken from:
+//1. add new event listener window, i used window. so i am accessing the whole page
+//2. create the event being the 'keydown' element, play sound being the result
+//3. then we choose which key will be the trigger through the '=, "key" is trigger which will be all keys as i have created a class in html
+//4. then to adjust the time for the function, being 0 seconds 
+
+//In order to actually add the music to the page, I had to create a document event listener
+//similar to the window, we had the function which is play sound
+//we then add what will happen if keys are pressed (keycode) which will be audio, linked as data-key in html.
+//I also added a div class called playing, which will be triggered through the click
+//similar to the window evetn listener, we have to add a time this function will last for which needs to match our window event listener
+//
+//https://keycode.info/
+//https://www.youtube.com/watch?v=5-koI06rmcA&t=109s
+//https://learn.framevr.io/project5/part2
